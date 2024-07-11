af8528c56d250a9e78bd710e20401f764e46ced5
darius <darius.cipleu@student.upt.ro>
2024-07-12 02:12:21 +0300

    isbn ex started

--- ISBN13_checker/main.c
+++ ISBN13_checker/main.c
@@ -1,8 +1,43 @@
+#include <stdint.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int check_no_length(uint64_t isbn) {
+  
+  int count = 0;
+  while (isbn != 0) {
+    isbn = isbn / 10;
+    count ++;
+  }
+  printf("count = %d", count);
+  return count;
+}
 
+int check_valid_isbn(uint64_t isbn) {
+  int sum = 0;
 
+  for(int i = 0; i < 13; i++) {
+    if()  
+  }
+}
+
+int main(int argc, char *argv[]) {
+  
+  uint64_t isbn;
+  int valid_isbn = 1;
+
+  
+  if (scanf("%13lu", &isbn) == 1) {
+    if (check_no_length(isbn) != 13) {
+      printf("isbn has to have length 13! \n");
+      return 1;
+    }
+    
+  }
+  else {
+    printf("insert valid ISBN13 number !");
+  }
+  printf("isbn = %lu \n", isbn);
   return 0;
 }
+
+
